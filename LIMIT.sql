3f2016aac0a1587b2dd7ec6764da0fa3e2b79216
d.golovanov <dumyka1990@gmail.com>
2022-02-13 18:28:53 +0300

    text

---
+++ LIMIT.sql
@@ -0,0 +1,5 @@
+
+select * from product limit 2;
+ select * from product where id >1 limit 2;
+ select * from product where id <3 limit 2;
+ 
---
+++ UPDATE.sql
@@ -0,0 +1,4 @@
+use testcase;
+select * from product;
+update product set name = 'шар новогодний' where id > 3;
+delete from product where id > 3;
